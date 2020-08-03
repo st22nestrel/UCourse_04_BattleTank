71ae20eba3d9b4ec728bb16a231a4c870c4f0d5d
St22 <timotej.ponek@gmail.com>
2020-08-03 21:31:46 +0200

    Blueprint Variables - prepare for color changing crosshair

--- BattleTanks/Source/BattleTanks/Public/TankPlayerController.h
+++ BattleTanks/Source/BattleTanks/Public/TankPlayerController.h
@@ -17,11 +17,13 @@ class BATTLETANKS_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
-
 	virtual void BeginPlay() override; // virtual enables children of given class to be overriden
 
 	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	ATank* GetControlledTank() const;
 
 private:
 	// Start the tank moving the barrel so that a shot would hit where
