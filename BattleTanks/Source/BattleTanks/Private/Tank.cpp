23dba9a5ad998bcaab77fc68f5a11662d8d2a76b
St22 <timotej.ponek@gmail.com>
2020-08-04 14:49:59 +0200

    Constructor & BeginPlay timing

--- BattleTanks/Source/BattleTanks/Private/Tank.cpp
+++ BattleTanks/Source/BattleTanks/Private/Tank.cpp
@@ -13,6 +13,8 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s DONKEY: Tank C++ Construct"), *TankName);
 }
 
 void ATank::Fire() {
@@ -50,7 +52,7 @@ void ATank::AimAt(FVector HitLocation)
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
-	Super::BeginPlay();
+	Super::BeginPlay(); // Needed for BP Begin Play to run!
 	
 }
 
